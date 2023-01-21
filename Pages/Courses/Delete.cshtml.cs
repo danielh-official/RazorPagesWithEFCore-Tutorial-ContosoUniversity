bb3062a721c54352ce03e1608f628541c29acbb1
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-21 12:04:05 -0500

    Update Delete.cshtml.cs

--- Pages/Courses/Delete.cshtml.cs
+++ Pages/Courses/Delete.cshtml.cs
@@ -24,40 +24,21 @@ namespace ContosoUniversity.Pages.Courses
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.Courses == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var course = await _context.Courses.FirstOrDefaultAsync(m => m.CourseID == id);
+            Course = await _context.Courses
+                .AsNoTracking()
+                .Include(c => c.Department)
+                .FirstOrDefaultAsync(m => m.CourseID == id);
 
-            if (course == null)
+            if (Course == null)
             {
                 return NotFound();
-            }
-            else 
-            {
-                Course = course;
             }
             return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync(int? id)
-        {
-            if (id == null || _context.Courses == null)
-            {
-                return NotFound();
-            }
-            var course = await _context.Courses.FindAsync(id);
-
-            if (course != null)
-            {
-                Course = course;
-                _context.Courses.Remove(Course);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
         }
     }
 }
