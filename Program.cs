20aedc0a125c3f5969796a96f745503b3ab01c63
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-21 10:40:39 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,6 +24,15 @@ if (!app.Environment.IsDevelopment())
     app.UseMigrationsEndPoint();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<SchoolContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
